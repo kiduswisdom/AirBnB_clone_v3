9aae3cb76355ba8d6b51e6eed9e47eb7ba72729f
kiduswisdom <kidusjob@gmail.com>
2023-02-17 19:08:11 +0000

    add-files-and-fixed

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -67,61 +67,52 @@ test_db_storage.py'])
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
-    def test_get_db(self):
-        """Test dbstorage with basic data"""
-        stateObj = State(name="GokuState")
-        stateObj.save()
-        models.storage.save()
-        listObj = list(models.storage.all(State).values())[0].id
-        strObj = str(models.storage.all()['State.' + listObj])
-        self.assertNotEqual(strObj, None)
-
-    def test_classofoutput_get_count_db(self):
-        """Test count dbstorage"""
-        total = (models.storage.count())
-        self.assertEqual(type(total), int)
-        totalState = (models.storage.count(State))
-        self.assertEqual(type(totalState), int)
-        state_id0 = list(models.storage.all(State).values())[0].id
-        classGet = models.storage.get(State, state_id0)
-        self.assertEqual(str(type(classGet)), "<class 'models.state.State'>")
-
-    def test_get_fs(self):
-        """Test fstorage"""
-        stateObj = State(name="GokuState")
-        stateObj.save()
-        failTry = models.storage.get('State', 'failId')
-        self.assertEqual(failTry, None)
-        failTry2 = models.storage.get('failId', stateObj.id)
-        self.assertEqual(failTry2, None)
-
-    def test_count_fs(self):
-        """count fs"""
-        count = models.storage.count()
-        countClass = models.storage.count('State')
-        newState = State(name="GokuState")
-        newState.save()
-        newCount = models.storage.count()
-        newCountClass = models.storage.count('State')
-        self.assertEqual(count + 1, newCount)
-        self.assertEqual(countClass + 1, newCountClass)
-
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_all_returns_dict(self):
+    def allReturnDict(self):
         """Test that all returns a dictionaty"""
         self.assertIs(type(models.storage.all()), dict)
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_all_no_class(self):
+    def allNoClass(self):
         """Test that all returns all rows when no class is passed"""
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_new(self):
+    def new(self):
         """test that new adds an object to the database"""
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_save(self):
+    def save(self):
         """Test that save properly saves objects to file.json"""
+
+
+class TestDBStorage(unittest.TestCase):
+    """Test the DBStorage class"""
+
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
+                     "not testing db storage")
+    def test_get(self):
+        """Test that get returns specific object, or none"""
+        newState = State(name="New York")
+        newState.save()
+        newUser = User(email="bob@foobar.com", password="password")
+        newUser.save()
+        self.assertIs(newState, models.storage.get("State", newState.id))
+        self.assertIs(None, models.storage.get("State", "blah"))
+        self.assertIs(None, models.storage.get("blah", "blah"))
+        self.assertIs(newUser, models.storage.get("User", newUser.id))
+
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
+                     "not testing db storage")
+    def test_count(self):
+        """add new object to db"""
+        startCount = models.storage.count()
+        self.assertEqual(models.storage.count("Blah"), 0)
+        newState = State(name="Montevideo")
+        newState.save()
+        newUser = User(email="ralexrivero@gmail.com.com", password="dummypass")
+        newUser.save()
+        self.assertEqual(models.storage.count("State"), startCount + 1)
+        self.assertEqual(models.storage.count(), startCount + 2)
